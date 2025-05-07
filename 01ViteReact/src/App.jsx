099b1197987c9529156a67056922ead22ec92a3b
Sourav <souravmallick900@gmail.com>
2025-05-07 10:52:16 +0530

    Learned the best practices and the rendering

---
+++ 01ViteReact/src/App.jsx
@@ -0,0 +1,14 @@
+import New from "./New.jsx";
+
+function App() {
+  return (
+    <>
+      <New />
+      <h1>Mai Aaagya react padhne</h1>
+      Loo sikh gaya algo
+
+    </>
+  );
+}
+
+export default App;
---
+++ 01ViteReact/src/New.jsx
@@ -0,0 +1,12 @@
+
+function Hello(){
+    return(
+        <>
+        <h1>hello</h1>
+        <h3>KYa mai padhai karun</h3>
+        kYa hoga ter    
+        </>
+    )
+}
+
+export default Hello
